04128a3c556e675fd911b8dbc2fecdf1a30c6bcd
Anuj Kulkarni <kulkarnianuj10@gmail.com>
2026-08-03 21:56:45 +0530

    Delete Script.js

--- Script.js
+++
@@ -1,9 +0,0 @@
-
-function addPassword(){
-    let password = document.getElementById("Password")
-    let name = document.getElementById("username")
-
-    if(password.length < 8){
-        alter("Length of Password Is Less Than 8");
-    }
-}
