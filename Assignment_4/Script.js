7a0100dd97eb6c74571ef2d8e028873be83a2f37
Anuj-Kulkarni <kulkarninanuj10@gmail.com>
2026-08-21 19:09:51 +0530

    Updated Assignement 4

--- Assignment_4/Script.js
+++ Assignment_4/Script.js
@@ -1,9 +1,31 @@
+// Get the form
+let form = document.getElementById("loginForm");
 
-function addPassword(){
-    let password = document.getElementById("Password")
-    let name = document.getElementById("username")
 
-    if(password.length < 8){
-        alter("Length of Password Is Less Than 8");
+// When the user clicks Login
+form.addEventListener("submit", function(event) {
+
+    // Stop the page from refreshing
+    event.preventDefault();
+
+
+    // Get username and password
+    let username = document.getElementById("username").value;
+
+    let password = document.getElementById("password").value;
+
+
+    // Check password length
+    if (password.length < 8) {
+
+        alert("Password must contain at least 8 characters.");
+
     }
-}
+
+    else {
+
+        alert("Login Successful!");
+
+    }
+
+});
